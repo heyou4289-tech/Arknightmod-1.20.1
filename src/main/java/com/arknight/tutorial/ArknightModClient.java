6ecf2595b2f3ecf958648f780a5c2442e529cb89
heyoulc <lionel-2009@qq.com>
2025-12-22 16:07:34 +0800

    Initial commit

---
+++ src/main/java/com/arknight/tutorial/ArknightModClient.java
@@ -0,0 +1,10 @@
+package com.arknight.tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArknightModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
